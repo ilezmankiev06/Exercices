33dd0c2d70d394563ba2321c2e0a5a0f46a8bb53
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-12 17:57:07 +0100

    exo 3 final

--- 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/03_fizz_buzz/src/index.js
+++ 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/03_fizz_buzz/src/index.js
@@ -1,6 +1,17 @@
-function fizzBuzz() {
+function fizzBuzz(array) {
   // Code the function here.
+  const list = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0)
+      list.push("FizzBuzz");
+    else if (array[i] % 3 === 0)
+      list.push("Fizz");
+    else if (array[i] % 5 === 0)
+      list.push("Buzz");
+    else 
+      list.push(array[i]);
+  }
+  return list;
 }
-
 // Leave the line below untouched for tests to work properly.
 module.exports = fizzBuzz;
