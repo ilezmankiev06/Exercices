966e26f32b69d1f22a03861ec783893553cd7136
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-12 14:43:49 +0100

    exo 2 function jour 2 final

--- 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/02_range_function/src/index.js
+++ 02_programming/02_algorithms_day_2/e929e2114976c6ee965bb4d5f7dfe7aae0bb1675/02_range_function/src/index.js
@@ -1,6 +1,22 @@
-function range() {
+function range(number1, number2) {
   // Code the function here.
+  const monTableau = [];
+  let depart = number1;
+  if (number1 < number2) {
+    for (let i = 0; i <= (number2 - number1); i++) {
+      monTableau[i] = depart;
+      depart++;
+    }
+  } else if (number1 > number2) {
+    for (let i = 0; i <= (number1 - number2); i++) {
+      monTableau[i] = depart;
+      depart--;
+    }
+  } else {
+    monTableau.push(depart);
+  }
+  return monTableau;
 }
 
 // Do not remove last lines, it is for tests
-module.exports = range;
+module.exports = range; 
