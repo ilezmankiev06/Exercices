691b0b8cbc8b8fb09c742a5b099cfe4e9e4b1cfc
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-14 22:55:34 +0100

    exo jour 1 a jour

--- 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/04_play_with_for_loop/src/03_iteration.js
+++ 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/04_play_with_for_loop/src/03_iteration.js
@@ -2,3 +2,6 @@
 //
 // -  Create an array called `literalDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each values of the table.
+const literalDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+literalDigits.forEach(literalDigits => 
+  console.log(literalDigits));
