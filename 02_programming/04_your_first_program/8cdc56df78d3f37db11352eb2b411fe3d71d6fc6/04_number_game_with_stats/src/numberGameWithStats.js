6aa28ca4a836006142805fee72635192d6fe6f8a
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-16 20:15:49 +0100

    exo 4 GameWithStats program terminer

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
@@ -1,30 +1,35 @@
 function numberGameWithStats(reader, min = 1, max = 100) {
   // Code here
   let random = Math.round(Math.random() * (max - min)) + min;
-
+  let counter = 0;
   console.log("Welcome!\nYou have to find the right number, between 1 and 100!\nGood luck!!");
 
   function number1Callback(number1) {
+    //variables
     let number2 = parseInt(number1);
+    counter = counter + 1;
+
     if (Number.isInteger(number2)) {
       number1;
     } else {
       console.log("This was not a number");
-      reader.question("Enter a number: ", number1Callback);
+      reader.question(`${counter}: Enter a number: `, number1Callback);
     }
+    //condition si number est </> que random
     if (number2 > max || number2 < min) {
       console.log("The number is between 1 and 100");
-      reader.question("Enter a number: ", number1Callback);
+      reader.question(`${counter}: Enter a number: `, number1Callback);
     }
+    //condition =/</> que random
     if (number2 === random) {
-      console.log("You won!");
+      console.log(`${counter}: You won!`);
       reader.close();
     } else if (number2 > random) {
       console.log("Too high");
-      reader.question("Enter a number: ", number1Callback);
+      reader.question(`${counter}: Enter a number: `, number1Callback);
     } else if (number2 < random) {
       console.log("Too low");
-      reader.question("Enter a number: ", number1Callback);
+      reader.question(`${counter}: Enter a number: `, number1Callback);
     }
   }
   reader.question("Enter a number: ", number1Callback);
