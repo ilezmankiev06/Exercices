9d52e9f327b7408ef5e35f979321263793fe9edf
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-16 19:51:09 +0100

    exo square digits terminer

--- 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
@@ -1,12 +1,10 @@
 function squareDigits(number) {
   // Code here
   if (Number.isInteger(number)) {
-    if (number === 9129)
-      return 811481;
-    else 
-      return number * number;
+    return parseInt(number.toString().split("").map(number => (parseInt(number) * parseInt(number))).join(""));
+  } else {
+    throw "not a integer!";
   }
-  throw "not a integer!";
 }
 console.log(squareDigits(9129));
 // Leave the line below for tests to work
