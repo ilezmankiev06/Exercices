bcc2ea9d06d45eaa22a241e23bf7135a60a64c0a
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-17 23:51:50 +0100

    exo_4_factory_terminer

--- 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/03_coffee_machine/src/index.js
+++ 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/03_coffee_machine/src/index.js
@@ -1,8 +1,8 @@
-const machine = require("./coffeeMachine");
-
-machine.fillWithLitersOfCoffee(10);
-console.log(machine.litersOfCoffee); // => 10
-console.log(machine.expresso()); // => true
-console.log(machine.litersOfCoffee); // => 9.92
-console.log(machine.longCoffee()); // => true
-console.log(machine.litersOfCoffee); // => 9.77
+const lukeSkywalker = {
+    firstname: "Luke",
+    lastname: "Skywalker",
+    jedi: function () {
+      return this;
+    },
+  };
+  const theChosenOne = lukeSkywalker.jedi();
--- 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/04_human_factory/src/humanFactory.js
+++ 02_programming/05_this/4e633277d39d3a9348733e29940e34ef61183f7b/04_human_factory/src/humanFactory.js
@@ -1,10 +1,32 @@
 const humanFactory = (humanData = {}) => {
   // code here
+  const human = {
+    firstName: "John",
+    lastName: "Doe",
+    genre: "male",
+    job: "unemployed",
+    fullname: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    introduction: function () {
+      return `Hello! My name is ${this.fullname()}.`;
+    },
+  };
+  if (humanData.genre === "female" && humanData.firstName === undefined) {
+    human.firstName = "Jane";
+  }
+  return { ...human, ...humanData };
 };
+const humans = humanFactory({});
+console.log(humans);
 
 const createHumans = (humans) => {
   // code here
+  return humans.map(humanFactory);
 };
+
+const humans2 = createHumans([{ lastName: "Toto" }, { genre: "female" }]);
+console.log(humans2);
 
 module.exports = {
   humanFactory,
