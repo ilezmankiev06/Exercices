692fba152c237a7f6f6d989eb7bcc65a8aa8e66f
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-24 14:16:07 +0100

    exo 2 typecript jour 1 terminer

--- 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/02_typed_function/src/index.ts
+++ 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/02_typed_function/src/index.ts
@@ -1,5 +1,12 @@
-function handleTuple() {
-  // Code the function here.
+type tuple = [string,number];
+function handleTuple(tuple:[string,number]): void {
+  tuple.forEach(element => {
+    if (typeof element === "string")
+    console.log(element + `The type of ${element} value is string`);
+    if (typeof element === "number")
+    console.log(element + `The type of value ${element} is number`);
+  });
+  
 }
 
 // Leave the line below for tests to work properly.
