568407af9f19c69c35d79575c01270039c170d09
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-24 15:26:38 +0100

    exo 3 typescript jour 1 terminer

--- 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/03_typed_class/src/index.ts
+++ 02_programming/10_typescript_101/d7806418db17545e4bdeebe9186a600c561b20c7/03_typed_class/src/index.ts
@@ -1,6 +1,23 @@
 class Bird {
   // Write your code here.
-}
+  age: number;
+  constructor(age: number){
+    this.age = age;
+  }
 
+  sing(){
+    console.log("Cui cui");
+  }
+  fly(temps: number): void {
+    if (this.age <= 1) {
+      console.log("The bird is too young to fly");
+    } else if (this.age > 1 && this.age <=3) {
+      console.log(`The bird flew ${temps} meters in ${temps} seconds`);
+    } else if (this.age > 3) {
+      const ms2 = temps * 2;
+      console.log(`The bird flew ${ms2} meters in ${temps} seconds`);
+    }
+    }
+    }
 // Leave the line below for tests to work properly.
 export { Bird };
