1873c95ecf45414b6f0ac90769542ac78c54cfb3
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-03-24 23:53:06 +0100

    exo calculator mis a jour

--- 02_programming/08_Reboot_day_1/2bd2a813fc43ac5e741a903d557ea1cf5bf5888a/01_reboot_calculator/src/calculator.js
+++ 02_programming/08_Reboot_day_1/2bd2a813fc43ac5e741a903d557ea1cf5bf5888a/01_reboot_calculator/src/calculator.js
@@ -10,50 +10,67 @@
 function calculator(reader) {
   console.log("Welcome to your calculator!");
   const askFirstNumber = () => {
-    reader.question("Enter your first number: \n", (value1) => {
+    reader.question("Enter your first number: \n> ", (value1) => {
       if (checkNumber(value1) !== value1) {
         console.log("Not a number");
         askFirstNumber();
       } else {
-        reader.question("Enter the operation [+ * / -]: \n", (operation) => {
-          const askSecondNumber = () => {
-            reader.question("Enter your second number: \n", (value2) => {
-              if (checkNumber(value2) !== value2) {
-                console.log("Not a number");
-                askSecondNumber();
-              } else {
-                function calcul() {
-                  if (operation === "+") {
-                    let result = parseInt(value1) + parseInt(value2);
-                    console.log(result);
-                  } else if (operation === "*") {
-                    let result = parseInt(value1) * parseInt(value2);
-                    console.log(result);
-                  } else if (operation === "/") {
-                    let result = parseInt(value1) / parseInt(value2);
-                    console.log(result);
-                  } else if (operation === "-") {
-                    let result = parseInt(value1) - parseInt(value2);
-                    console.log(result);
+        const askOperation = () => {
+          reader.question("Enter the operation [+ * / -]: \n> ", (operation) => {
+            if (checkOperation(operation) !== operation) {
+              console.log("Not a operation\nPlease choose a correct operation between [ +  *  /  - ]");
+              askOperation();
+            } else {
+              const askSecondNumber = () => {
+                reader.question("Enter your second number: \n> ", (value2) => {
+                  if (checkNumber(value2) !== value2) {
+                    console.log("Not a number");
+                    askSecondNumber();
+                  } else {
+                    // eslint-disable-next-line no-inner-declarations
+                    function calcul() {
+                      if (operation === "+") {
+                        let result = parseInt(value1) + parseInt(value2);
+                        console.log(result);
+                      } else if (operation === "*") {
+                        let result = parseInt(value1) * parseInt(value2);
+                        console.log(result);
+                      } else if (operation === "/") {
+                        let result = parseInt(value1) / parseInt(value2);
+                        console.log(result);
+                      } else if (operation === "-") {
+                        let result = parseInt(value1) - parseInt(value2);
+                        console.log(result);
+                      }
+                      reader.close();
+                    }
+                    calcul();
                   }
-                  reader.close();
-                }
-                calcul();
-              }
-            });
-          };
-          askSecondNumber();
-        });
-      }
+                });
+              };
+              askSecondNumber();
+            };
+          });
+        };
+        askOperation();
+      };
     });
   };
   askFirstNumber();
-}
+};
+
 function checkNumber(value) {
   if (isNaN(value)) {
     return null;
   } else {
     return value;
+  }
+}
+function checkOperation(operation) {
+  if (operation !== "+" && operation !== "*" && operation !== "-" && operation !== "/") {
+    return null;
+  } else {
+    return operation;
   }
 }
 
