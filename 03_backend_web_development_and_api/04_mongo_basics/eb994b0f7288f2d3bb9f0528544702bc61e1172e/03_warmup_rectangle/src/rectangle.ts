ed868ce9406d22c32aab87045e3bba9a02e801fd
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-01 15:06:36 +0200

    exo_3_rectangle_mongo_terminer

--- 03_backend_web_development_and_api/04_mongo_basics/eb994b0f7288f2d3bb9f0528544702bc61e1172e/03_warmup_rectangle/src/rectangle.ts
+++ 03_backend_web_development_and_api/04_mongo_basics/eb994b0f7288f2d3bb9f0528544702bc61e1172e/03_warmup_rectangle/src/rectangle.ts
@@ -1,2 +1,14 @@
-export function rectangle(width: number, height: number): string {
+export function rectangle(width: number, height: number): string { 
+
+    const result: string[] = [];
+    let res: string = "";
+
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        res += "*";
+    }
+    result.push(res);
+    res = "";
+    }
+    return result.join("\n");
 }
