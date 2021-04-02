12e83b33bcfc262bc5fa1f2b0c289870ec483ad5
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-02 11:02:54 +0200

    exo_0_mongo_reboot_terminer

---
+++ 03_backend_web_development_and_api/05_mongo_reboot/ba84a3f9f084e037555e3138d1fd8d2f5c8c5155/01_filter_games/src/games.ts
@@ -0,0 +1,4 @@
+import { Game } from "./types";
+
+export function filterGameByPlatform(games: Game[], platform: string): Game[] {
+}
---
+++ 03_backend_web_development_and_api/05_mongo_reboot/ba84a3f9f084e037555e3138d1fd8d2f5c8c5155/03_line/src/line.ts
@@ -0,0 +1,2 @@
+export function line(numberOfStars: number): string {
+}
