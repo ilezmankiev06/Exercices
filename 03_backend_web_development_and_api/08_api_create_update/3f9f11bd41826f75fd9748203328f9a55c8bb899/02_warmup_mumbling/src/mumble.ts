5561c35c2dbeb05dc83fabf6a7835ad4c359f0f2
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-15 09:33:56 +0200

    exo_api_create_update

--- 03_backend_web_development_and_api/08_api_create_update/3f9f11bd41826f75fd9748203328f9a55c8bb899/02_warmup_mumbling/src/mumble.ts
+++ 03_backend_web_development_and_api/08_api_create_update/3f9f11bd41826f75fd9748203328f9a55c8bb899/02_warmup_mumbling/src/mumble.ts
@@ -1,11 +1,18 @@
-export function mumble(mumbled: string): string {
-    const tab = []
-    mumbled.split("").forEach((element, index) => {
-    let letter = element
-    for (let i = 0; i < index; i++) {
-      letter += element
+//cree un tableau vide 
+//separer chaque lettre et stocker dans un tableau
+//pour chaque element les dupliquer selon son index
+//push dans le tableau vide 
+//mettre en miniscule
+// return
+
+export function mumble(input: string): string {
+  const tableau = []
+  input.split("").forEach((element, index) => {
+    for (let i = -1; i < index; i++) {
+      tableau.push(element)
     }
-    tab.push(letter);
-    })
-    return tab.join("-").toLowerCase();
+      tableau.push("-")
+  })
+  tableau.pop()
+  return tableau.join("").toLowerCase();
 }
