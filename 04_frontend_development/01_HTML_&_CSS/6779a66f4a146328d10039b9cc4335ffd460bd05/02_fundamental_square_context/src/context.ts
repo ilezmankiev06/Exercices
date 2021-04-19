f2bd47910dbd72eda3bcdb2f5e83a4189d1fbebd
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-19 17:49:05 +0200

    exo_2_square_context_terminer

--- 04_frontend_development/01_HTML_&_CSS/6779a66f4a146328d10039b9cc4335ffd460bd05/02_fundamental_square_context/src/context.ts
+++ 04_frontend_development/01_HTML_&_CSS/6779a66f4a146328d10039b9cc4335ffd460bd05/02_fundamental_square_context/src/context.ts
@@ -1,11 +1,9 @@
 import {squareNumber} from "./squareNumber"
 
 export function context(arrayOfNumbers: number[]): number[] {
-  squareNumber(5);
-  squareNumber(4);
-  squareNumber(3);
-  squareNumber(2);
-  squareNumber(1);
-  const squaredDigits = arrayOfNumbers.map(n => n * n)
-  return squaredDigits;
+  // Code the function here
+  const array = arrayOfNumbers.map((element) => {
+     return squareNumber(element)
+  })
+  return array;
 }
