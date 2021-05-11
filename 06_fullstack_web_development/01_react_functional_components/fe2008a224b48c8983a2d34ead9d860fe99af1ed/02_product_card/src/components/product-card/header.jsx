edefe7b3f9db8bdbe74838500c743f7c3a9c35f6
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-05-11 09:38:38 +0200

    exo 2 product card react pas encore terminer

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/header.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const CardHeader = () => {
-  return <div />;
+const CardHeader = (props) => {
+  console.log(props);
+  return (
+    <>
+      {props.game.name}
+      <div>
+        {props.game.platforms.map(({ platform_logo }, index) => {
+          return <img key={index} src={platform_logo.url} alt="" />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CardHeader;
